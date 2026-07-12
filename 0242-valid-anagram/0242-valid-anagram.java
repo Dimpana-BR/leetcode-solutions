bc24b41eddec5a34786efb928cb3fbcbee24ba19
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-07-12 07:22:03 +0530

    Time: 20 ms (9.11%), Space: 46.6 MB (25.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> map = new HashMap<>();
-       
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-                
-            }else{
-                map.put(ch,1);
-            }
+       for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(map.containsKey(ch)){
+            map.put(ch,map.get(ch)+1);
+        }else{
+            map.put(ch,1);
         }
-        for(int j=0;j<t.length();j++){
-            char th=t.charAt(j);
-            if(!map.containsKey(th)){
-                return false;
-            }
-            map.put(th,map.get(th)-1);
-            if(map.get(th)==0){
-                map.remove(th);
-            }
+       }
+       for(int j=0;j<t.length();j++){
+        char th=t.charAt(j);
+        if(!map.containsKey(th)){
+            return false;
         }
-        if(map.isEmpty()){
-            return true;
+        map.put(th,map.get(th)-1);
+        if(map.get(th)==0){
+            map.remove(th);
         }
-        return false;
+
+       }
+       if(map.isEmpty()){
+        return true;
+       }
+       return false;
+
+      
     }
 
 }
