f58fda51e1517c9ce370884c3b6ca70d254710f7
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-07-12 13:42:57 +0530

    Time: 19 ms (7.73%), Space: 47.2 MB (13.03%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }else{
+                map.put(ch,1);
+            }
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char th=ransomNote.charAt(i);
+            if(!map.containsKey(th)){
+                return false;
+            }
+            
+            map.put(th,map.get(th)-1);
+            if(map.get(th)==0){
+                map.remove(th);
+            }
+        }
+        return true;
+    }
+}
