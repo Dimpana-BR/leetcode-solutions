0d69d69e8cdf5dda112ee0837b9d0f15302923ca
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-07-12 07:15:54 +0530

    Time: 19 ms (10.83%), Space: 46.6 MB (25.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+       
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+                
+            }else{
+                map.put(ch,1);
+            }
+        }
+        for(int j=0;j<t.length();j++){
+            char th=t.charAt(j);
+            if(!map.containsKey(th)){
+                return false;
+            }
+            map.put(th,map.get(th)-1);
+            if(map.get(th)==0){
+                map.remove(th);
+            }
+        }
+        if(map.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+}
