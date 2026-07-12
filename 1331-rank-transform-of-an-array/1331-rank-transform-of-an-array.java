c4ecfd1136bf012a02bca0c36c787a0553334b91
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-07-12 12:43:10 +0530

    Time: 28 ms (93.14%), Space: 77 MB (31.99%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] temparr=arr.clone();
-        Arrays.sort(temparr);
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int rank=1;
-        for(int i=0;i<temparr.length;i++){
-            if(!map.containsKey(temparr[i])){
-             map.put(temparr[i],rank);
-             rank++;
+        int[] temp=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            temp[i]=arr[i];
+        }
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> map=new HashMap<>();
+       int  rank=1;
+        for(int i:temp){
+            if(!map.containsKey(i)){
+                map.put(i,rank);
+                rank++;
             }
         }
-    for(int i=0;i<arr.length;i++){
+       for(int i=0;i<arr.length;i++){
         arr[i]=map.get(arr[i]);
-    }
-    return arr;
+       }
+        return arr;
     }
 }
