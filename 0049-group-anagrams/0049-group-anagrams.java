f30532f011484154fe7165d4c0c8fc3a16754849
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-07-28 18:36:47 +0530

    Time: 7 ms (72.96%), Space: 49.4 MB (92.7%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<String,List<String>> map = new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String word = strs[i];
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<String>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
