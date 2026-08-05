0039df64aeec1030a17f18578c7c455e314ea557
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-08-05 14:02:28 +0530

    Time: 3 ms (80.91%), Space: 43.4 MB (82.61%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -9,19 +9,18 @@ class Solution {
                 stack.push(stack.peek()*2);
             }else if(op.equals("+")){
                 int first=stack.pop();
-                int second = stack.peek();
+                int second=stack.peek();
                 stack.push(first);
                 stack.push(first+second);
-            } else{
-                int num = Integer.parseInt(op);
-                stack.push(num);
+            }else{
+                int sum =Integer.parseInt(op);
+                stack.push(sum);
             }
         }
-
-        int sum=0;
+        int finals=0;
         while(!stack.isEmpty()){
-            sum=sum+stack.pop();
+            finals=finals+stack.pop();
         }
-        return sum;
+        return finals;
     }
 }
