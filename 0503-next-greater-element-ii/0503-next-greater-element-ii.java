2c7636648a5cb8e13b2fac838195141ce92b7b2e
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-08-08 10:44:41 +0530

    Time: 15 ms (61.47%), Space: 48.7 MB (23.19%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,9 +3,9 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         Stack<Integer> stack = new Stack<>();
-        for(int i=2*n-1;i>=0;i--){
-            int index= i%n;
-            while(!stack.isEmpty() && stack.peek() <= nums[index]){
+        for(int i=2*n -1;i>=0;i--){
+            int index=i%n;
+            while(!stack.isEmpty() && stack.peek()<=nums[index]){
                 stack.pop();
             }
             if(i<n){
