a9dc0fec5a42a756cd499df811d60e2c26636266
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-08-25 10:34:41 +0530

    Time: 0 ms (100%), Space: 43.3 MB (90.78%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,7 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int left=0;
         for(int right=0;right<nums.length;right++){
-            if(nums[right]!= val){
+            if(nums[right]!=val){
                 nums[left]=nums[right];
                 left++;
             }
