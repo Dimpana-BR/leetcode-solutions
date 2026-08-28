6be38d3f87d5bf9bf4ab899fbdc7f6788034febc
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-08-28 18:45:56 +0530

    Time: 2 ms (82.97%), Space: 43.3 MB (45.65%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -5,7 +5,7 @@ class Solution {
         String vowels = "aeiouAEIOU";
         for(int i=0;i<words.length;i++){
             String word = words[i];
-            if(vowels.indexOf(word.charAt(0))!=-1){
+            if(vowels.indexOf(word.charAt(0))!= -1){
                 result.append(word);
             }else{
                 result.append(word.substring(1));
@@ -15,7 +15,7 @@ class Solution {
             for(int j=0;j<=i;j++){
                 result.append("a");
             }
-            if(i!= words.length-1){
+            if(i != words.length-1){
                 result.append(" ");
             }
         }
