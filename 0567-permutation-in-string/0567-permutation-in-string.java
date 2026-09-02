64fd58cfd9fcf91105af4df9379fc13bc6d25d63
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-09-02 14:29:00 +0530

    Time: 7 ms (68.19%), Space: 43.8 MB (93.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length())
-        return false;
-        int[] s1freq = new int[26];
-        int[] windowfreq = new int[26];
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int[] s1freq=new int[26];
+        int[] windowcount = new int[26];
         for(int i=0;i<s1.length();i++){
             s1freq[s1.charAt(i)-'a']++;
-            windowfreq[s2.charAt(i)-'a']++;
+            windowcount[s2.charAt(i)-'a']++;
         }
-        if(Arrays.equals(s1freq,windowfreq)){
+        if(Arrays.equals(s1freq,windowcount)){
             return true;
         }
         for(int right=s1.length();right<s2.length();right++){
-            windowfreq[s2.charAt(right)-'a']++;
-            windowfreq[s2.charAt(right-s1.length())-'a']--;
-            if(Arrays.equals(s1freq,windowfreq)){
+            windowcount[s2.charAt(right)-'a']++;
+            windowcount[s2.charAt(right-s1.length())-'a']--;
+            if(Arrays.equals(s1freq,windowcount)){
                 return true;
             }
         }
