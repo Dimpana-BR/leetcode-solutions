68e5ae2635501288ceb939f8d91171e678d99995
Dimpana-BR <dimpanadimpu18@gmail.com>
2026-09-03 16:58:54 +0530

    Time: 2 ms (91.84%), Space: 48.2 MB (6.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left=0;
-        for(int right=0;right<nums.length;right++){
-            if(nums[right]!=0){
-                swap(left,right,nums);
-                left++;
-            }
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                swap(nums,i,j);
+                j++;
+            }           
         }
     }
-    public void swap(int left,int right,int[] nums){
-        int temp=nums[left];
-        nums[left]=nums[right];
-        nums[right]=temp;
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
